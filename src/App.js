ea475c244eb663249b270a6e8fe74661a7b4df27
Alex Dogoum <lexhoo@yahoo.fr>
2019-04-10 14:05:05 +0200

    ajout du  banner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Banner from './Banner';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+      
+      <Banner />
+
       </div>
+
     );
   }
 }
