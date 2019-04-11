f17e53c08f932f2c0c14eaad0e1e81ba5fd5df50
Spliit <mcuville@gmail.com>
2019-04-11 14:12:53 +0200

    fin page 2 prestaform

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Prestaform2 from "./prestaform2/Prestaform2";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Prestaform2 />
       </div>
     );
   }
---
+++ src/prestaform2/Slideform.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+import './Slideform.css';
+
+export default class Slideform extends React.Component {
+  render() {
+    return (
+      <Pagination size="lg" className="slide" aria-label="nav formslide">
+        <PaginationItem>
+          <PaginationLink className="slide1" href="#">
+            1
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink className="slide2" href="#">
+            2
+          </PaginationLink>
+        </PaginationItem>
+        </Pagination>
+    );
+  }
+}
