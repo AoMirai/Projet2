ef64f92d058a4f1d85bec0a642a407512326991e
Spliit <mcuville@gmail.com>
2019-04-12 10:57:14 +0200

    import pp

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navigation from "./NavbarFooter/Navbar";
 import Footer from "./NavbarFooter/Footer";
 import Prestaform1 from "./PrestaForm1/PrestaForm";
 import Prestaform2 from "./prestaform2/Prestaform2";
+import Profil from './PageProfil/Profil'
 
 class App extends Component {
   render() {
@@ -17,6 +18,7 @@ class App extends Component {
             <Route exact path="/"/>
             <Route path="/newpresta/1" component={Prestaform1} />
             <Route path="/newpresta/2" component={Prestaform2} />
+            <Route path="/profil/1" component={Profil} />
           </Switch>
           <Footer />
         </BrowserRouter>
