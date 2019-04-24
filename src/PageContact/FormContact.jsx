6db117d54e197d875b24c21bfa31bfca510cee70
Alex Dogoum <lexhoo@yahoo.fr>
2019-04-24 17:44:25 +0200

    petits ajustements

--- src/PageContact/FormContact.jsx
+++ src/PageContact/FormContact.jsx
@@ -1,48 +1,38 @@
 import React, { Component } from "react";
-import './FormContact.css'
+import { InputGroup, InputGroupAddon, FormGroup, Label, Input, Button } from 'reactstrap';
 
-class FormContact extends Component {
-    state ={
-        Name: "",
-        Surname:"",
-        Email: "",
-        Commentaire:""
-    }
-
-handleChange = event => {
-    this.setState({Name: event.target.value , Surname : event.target.value, Mail : event.target.value, Commentaire: event.target.value })
-}
-
-
-
-
-    render() {
-        return(
-            <form onSubmit={this.submitForm}>
-            <div className="Form">
-            
-            Name: {this.state.name}
-            <input name = "name" type="texte"  />
-            <br/>
-            Surname: {this.state.Surname}
-            <input surname = "Surname" type="texte"  />
-            <br/>
-            Mail: {this.state.Mail}
-            <input Mail = "Mail" type="texte"  />
-            <br/>
-            Comment: {this.state.Commentaire}
-            <textarea col={40} rows={10}  />
-
-            <div classename="form2"/>
-          <input type="button" name="bouton" value='Send !' onclick="this.value='Touché !"/>
-
-             </div>
-             </form>
-             
-            
-        )
-    }
-}
-
+const FormContact = (props) => {
+  return (
+    <div className="Form">
+    <div className="a">
+      <InputGroup>
+        <InputGroupAddon addonType="prepend"></InputGroupAddon>
+        <Input placeholder="Name :" />
+      </InputGroup>
+      </div>
+      <br />
+      <div className="b">
+      <InputGroup>
+        <InputGroupAddon addonType="prepend"></InputGroupAddon>
+        <Input placeholder="Surname :" />
+      </InputGroup>
+      </div>
+      <br />
+      <div className="c">
+        <InputGroup>
+        <InputGroupAddon addonType="prepend"></InputGroupAddon>
+        <Input placeholder="@ :" />
+      </InputGroup>
+      </div>
+      <br />
+      <FormGroup>
+          <Label for="exampleText">Votre Commentaire :</Label>
+          <Input type="Votre Commentaire :" name="text" id="exampleText" />
+        </FormGroup>
+        <div className="buton"><Button color="primary">primary</Button>{' '}</div>
+    </div>
+    
+  )
+  }
 
 export default FormContact;
