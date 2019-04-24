c103874a0fa9d4e538212171f7f3d8f94074d061
Alex Dogoum <lexhoo@yahoo.fr>
2019-04-24 11:48:16 +0200

    ajout form

--- src/PageContact/FormContact.jsx
+++ src/PageContact/FormContact.jsx
@@ -1,36 +1,69 @@
 import React, { Component } from "react";
+import './FormContact.css'
 
 class FormContact extends Component {
-    constructor(props){
-        super(props);
-        this.state = { value : props.value};
+    state ={
+        Name: "",
+        Surname:"",
+        Email: "",
+        Commentaire:""
     }
-    handlerChange(event) {
-        this.setState({value : event.target.value});
-    }
-    handlerFocus(event){
-        this.setState({value : ""});
-    }
-    handlerKeyPress(event){
-        if (event.shiftkey && event.charcode === 13) {
-            var value = event.target.value;
-            this.props.onValid(value);
-        }
-    }
-    render(){
-        return(
 
-        <textarea cols={this.props.cols}
-        rows={this.props.rows}
-        value={this.state.value}
-        onChange={this.handlerChange.bind(this)}
-        onFocus={this.handlerFocus.bind(this)}
-        onKeysPress={this.handlerKeyPress.bind(this)}
-        autoFocus={this.props.focus} />
+handleChange = event => {
+    this.setState({Name: event.target.value , Surname : event.target.value, Mail : event.target.value, Commentaire: event.target.value })
+}
+
+onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+   }
+
+   submitForm(e) {
+    const config = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(this.state),
+       };
+       const url = "http://51.68.18.101:3002/diggnshare/api/recipients";
+    
+       fetch(url, config)
+    .then(res => res.json())
+     .then(res => {
+       
+     });
+    e.preventDefault();
+   }
+
+
+    render() {
+        return(
+            <form onSubmit={this.submitForm}>
+            <div className="Form">
+            
+            Name: {this.state.name}
+            <input name = "name" type="texte"  />
+            <br/>
+            Surname: {this.state.Surname}
+            <input surname = "Surname" type="texte"  />
+            <br/>
+            Mail: {this.state.Mail}
+            <input Mail = "Mail" type="texte"  />
+            <br/>
+            Comment: {this.state.Commentaire}
+            <textarea col={40} rows={10}  />
+
+            <div classename="form2"/>
+          <input type="button" name="bouton" value='Send !' onclick="this.value='Touché !"/>
+
+             </div>
+             </form>
+             
+            
         )
     }
-
-
 }
 
 
--- src/PageContact/PageContact.jsx
+++ src/PageContact/PageContact.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import FormContact from './FormContact';
+import Titre from './Titre';
+import './FormContact.css'
+
 
 class PageContact extends Component {
     render() {
         return(
+            <div className="PageContact">
+            <Titre />
             <FormContact />
+            
+            </div>
         )
     }
 }
