f5c9e2c1076c814aecc466357f4c73fb3e3c9c95
Chris Fradet <chris.fradet.fac@gmail.com>
2019-04-29 16:44:10 +0200

    fix

--- src/Accueil/Bio.jsx
+++ src/Accueil/Bio.jsx
@@ -5,8 +5,10 @@ class Bio extends Component {
     render() {
         return ( 
             <div className= "Bio">
+                <h1>Qui sommes nous ?</h1>
                     <p>	Avec Digg'n Share, les professionnels de la musique s’invitent chez vous !</p>
                     <p> Bookez dès maintenant vos artistes et collectifs préférés du moment </p>
+                <h1>Focus on</h1>
             </div>
             )
     }
