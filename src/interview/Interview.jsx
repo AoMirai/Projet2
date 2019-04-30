714190b03c396b5454ea903b7a17986583e4ea7e
Audrey Brethome <audrey.brethome@gmail.com>
2019-04-30 12:06:36 +0200

    correction interview

--- src/interview/Interview.jsx
+++ src/interview/Interview.jsx
@@ -5,7 +5,8 @@ import  './Interview.css'
 class Interview extends Component {
     render() {
         return (
-            <div className="Focus row"> 
+            <div className="Interview">
+            <div className="focus row"> 
                 <img className="img1 col-xs-5 col-md-5 col-lg-5" src="image/img-interview.jpg" alt="concert" height="300" width="00px"/>
                 <p className="text col-xs-12 col-md-12 col-lg-7" ><span>A</span>ctifs depuis de nombreuses années sur Bordeaux, les membres du collectif tplt font vivre la scène électronique et la ville à leur manière. Désireux de défendre une ligne artistique à leur image et d’inviter les artistes qu’ils apprécient, le crew trouve un écrin idéal au coeur des Vivres de l’Art. Alors à l’origine des concepts Le Verger/La Serre, tplt anime régulièrement les samedis après-midi des bordelais. Le collectif propose en parallèle des événements inédits en journée et en soirée. À la recherche de fraicheur, ils veulent diversifier la scène et mettre en émulsion les talents locaux. Ils sont également co-fondateur de Déviante, qui propose des événements où la musique qui file pas droit est reine. L’histoire de tplt démarre en octobre 2012. Charl, Superlate, Yougo et Thibault entendent alors défendre une certaine ligne artistique ainsi que fédérer autour d’eux les artistes d’une scène locale parfois un peu dispersée. Avec la création du Bootleg en 2013, le collectif trouve rapidement un lieu adéquat pour la mise en place de leur soirées.</p>
 
@@ -14,6 +15,7 @@ class Interview extends Component {
                 <span>C</span>’est d’abord Le Verger et La Serre, deux événements ayant su imposer une nouvelle rythmique festive en ville, avec des teufs organisées en après-midi aux Vivres de l’Art. Avec le succès significatif rencontré par le concept, le collectif s’est même essayé à l’exercice du festival, en 2017, avec un casting impeccable de diggers européens et d’ailleurs (Tornado Wallace, Orpheu The Wizard, …). On peut également retrouver tplt derrière les soirées Déviante, consacrées aux musiques « qui ne filent pas droit », généralement programmés au Void.</p>
                 
                 <Carousel />
+            </div>
             </div>
         )
     }
