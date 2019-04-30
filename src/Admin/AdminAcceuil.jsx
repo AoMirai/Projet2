28485221c10716216702fd4fedd0292ad91acb36
Julie Charlemagne <Julie.Charlemagne@live.fr>
2019-04-30 15:48:32 +0200

    style messages

--- src/Admin/AdminAcceuil.jsx
+++ src/Admin/AdminAcceuil.jsx
@@ -18,7 +18,7 @@ class AdminAcceuil extends Component {
     render() { 
         return(
          <div className="AdminAcceuil">
-            <h3>Bienvenue, Administrateur !</h3>
+            <h2>Bienvenue, Administrateur !</h2>
             <p>Vous avez {this.state.demandes.length} demandes en attentes.
             Et 5 messages non-lus.
             </p>
